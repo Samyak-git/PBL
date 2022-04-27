ad285848da212b5953fdcd79930ab464698a9ba6
SamyakJain2020 <samyakjainking@gmail.com>
2022-04-27 15:27:56 +0530

    added home page

--- pages/home.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-
-export default () => {
-  return <Navbar location="1" />
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,91 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import App from '../components/App'
+import BannerImage from '../images/HeroBanner.jpg'
 const Home: NextPage = () => {
   return (
-    <div>
-   <App></App>
+    <div className="relative bg-white overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
+
+          <div>
+            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+              <nav
+                className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                aria-label="Global"
+              >
+                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                  <div className="flex items-center justify-between w-full md:w-auto">
+                    <div className="-mr-2 flex items-center md:hidden">
+                      <button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-orange-500">
+                        <span className="sr-only">Open main menu</span>
+                        {/* <MenuIcon className="h-6 w-6" aria-hidden="true" /> */}
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+            </div>
+          </div>
+          
+          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <div className="text-4xl flex flex-col tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <div className="block xl:inline font-mw">
+                  Feed the hungry, Save humanity.
+                </div>
+                <div className="block text-orange-600 xl:inline">
+                  One More Meal
+                </div>
+              </div>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                There is more than enough food produced in the world to feed
+                everyone on the planet. But still as many as 811 million people
+                worldwide go to bed hungry each night.
+              </p>
+              <div className="mt-5 sm:mt-8 flex flex-col sm:justify-center sm:flex-row lg:justify-start">
+                <button
+                 
+                  className="rounded-md shadow"
+                >
+                  <a
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-600 hover:bg-orange-700 md:py-4 md:text-lg md:px-10"
+                  >
+                    Notify Us
+                  </a>
+                </button>
+                <button
+                  className="mt-3 sm:mt-0 sm:ml-3"
+                >
+                  <a
+                    href="#"
+                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-orange-700 bg-orange-100 hover:bg-orange-200 md:py-4 md:text-lg md:px-10"
+                  >
+                    Donate Meal
+                  </a>
+                </button>
+              </div>
+            </div>
+          </main>
+        </div>
+        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+          <img
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            src={BannerImage.src}
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   )
 }
